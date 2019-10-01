802a2b37adfaf67cdc3a90b87a40748d26be8885
Robin Lack <lack@puzzle.ch>
2019-10-01 18:43:21 +0200

    initialized view

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,95 @@
 import React from "react";
+import Container from "@material-ui/core/Container";
+import clsx from 'clsx';
+import Box from "@material-ui/core/Box";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Collapse from '@material-ui/core/Collapse';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from "@material-ui/core/styles";
+import IconButton from '@material-ui/core/IconButton';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { render } from "react-dom";
-import { Pet } from "./Pet";
-import { SearchParams } from "./SearchParams";
+import { Doughnut } from 'react-chartjs-2';
+
+const useStyles = makeStyles(theme => ({
+  card: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+}));
 
 const App = () => {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+  const [chartValue, setChartValue] = React.useState(20);
+  
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  const chartData = {
+    datasets: [{
+        data: [chartValue,(chartValue-100)*(-1)],
+        backgroundColor : [
+          "orange"
+        ]
+    }],
+
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+        'Full',
+        "Empty"
+    ]
+    
+
+};
   return (
-    <div>
-      <h1>Adopt me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Chiuaua"></Pet>
-      <Pet name="Fido" animal="Dog" breed="Boxer"></Pet>
-      <Pet name="Pepper" animal="Cat" breed="Siamese"></Pet>
-    </div>
+    <Container maxWidth="sm">
+      <Box component="span" m={1}>
+        <Card raised>
+          <CardContent>
+          <Typography variant="body1" color="textPrimary" component="p">
+            Bern Bahnhof #1
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Recycling
+          </Typography>
+          <Doughnut data={chartData}/>
+          </CardContent>
+          <CardActions>
+            <IconButton
+              className={clsx(classes.expand, {
+                [classes.expandOpen]: expanded
+              })}
+              onClick={handleExpandClick}
+              aria-expanded={expanded}
+              aria-label="show more"
+            >
+              <ExpandMoreIcon />
+            </IconButton>
+          </CardActions>
+          <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+              
+        </CardContent>
+      </Collapse>
+        </Card>
+      </Box>
+    </Container>
   );
 };
 
-render(<SearchParams />, document.getElementById("root"));
-//render(<App />, document.getElementById("root"));
-
+render(<App />, document.getElementById("root"));
