629cf7169c3b2a13118ba83d8090fc236a382b0d
Robin Lack <lackrobin@gmail.com>
2019-10-01 22:24:19 +0200

    removed leftovers and extracted card

---
+++ WasteCard.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import clsx from 'clsx';
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Collapse from '@material-ui/core/Collapse';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from "@material-ui/core/styles";
+import IconButton from '@material-ui/core/IconButton';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { Doughnut } from 'react-chartjs-2';
+
+const useStyles = makeStyles(theme => ({
+    card: {
+        maxWidth: 345,
+    },
+    media: {
+        height: 0,
+        paddingTop: '56.25%', // 16:9
+    },
+    expand: {
+        transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+        }),
+    },
+}));
+export const WasteCard = ({ data }) => {
+    const classes = useStyles();
+    const [expanded, setExpanded] = React.useState(false);
+    const [chartValue, setChartValue] = React.useState(20);
+
+    const handleChartValue = () => {
+        setChartValue(chartValue + 1);
+    }
+
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+
+    const chartData = {
+        datasets: [{
+            data: [chartValue, (chartValue - 100) * (-1)],
+            backgroundColor: [
+                "orange"
+            ]
+        }],
+        labels: [
+            'Full',
+            "Empty"
+        ]
+
+
+    };
+    return (
+        <Card raised className="card">
+            <CardContent>
+                <Typography variant="body1" color="textPrimary" component="p">
+                    {data.name}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    {data.types[0]}
+                </Typography>
+                <Button onClick={handleChartValue}>Add trash</Button>
+                <Doughnut data={chartData} />
+            </CardContent>
+            <CardActions>
+                <IconButton
+                    className={clsx(classes.expand, {
+                        [classes.expandOpen]: expanded
+                    })}
+                    onClick={handleExpandClick}
+                    aria-expanded={expanded}
+                    aria-label="show more"
+                >
+                    <ExpandMoreIcon />
+                </IconButton>
+            </CardActions>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+                <CardContent>
+
+                </CardContent>
+            </Collapse>
+        </Card>);
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,94 +1,55 @@
 import React from "react";
-import Container from "@material-ui/core/Container";
-import clsx from 'clsx';
-import Box from "@material-ui/core/Box";
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import Collapse from '@material-ui/core/Collapse';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from "@material-ui/core/styles";
-import IconButton from '@material-ui/core/IconButton';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import { render } from "react-dom";
-import { Doughnut } from 'react-chartjs-2';
+import Grid from '@material-ui/core/Grid';
 
-const useStyles = makeStyles(theme => ({
-  card: {
-    maxWidth: 345,
-  },
-  media: {
-    height: 0,
-    paddingTop: '56.25%', // 16:9
-  },
-  expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
-    }),
-  },
-}));
+import { render } from "react-dom";
+import { WasteCard } from "../WasteCard";
+
+
 
 const App = () => {
-  const classes = useStyles();
-  const [expanded, setExpanded] = React.useState(false);
-  const [chartValue, setChartValue] = React.useState(20);
+  let data1={
+    name: "Bern Banhof 1",
+    types: ["Trash"],
+    full: [20]
+  }
+  let data2={
+    name: "Bern Banhof 2",
+    types: ["Trash", "Paper", "Glass", "Pet"],
+    full: [20, 40, 70, 50]
+  }
+  let data3={
+    name: "Bern Banhof 3",
+    types: ["Batteries", "Paper", "Glass", "Pet"],
+    full: [90, 40, 70, 50]
+  }
+  let data4={
+    name: "Bern Banhof 4",
+    types: ["Batteries", "Paper", "Glass", "Pet"],
+    full: [90, 40, 70, 50]
+  }
   
-
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
-
-  const chartData = {
-    datasets: [{
-        data: [chartValue,(chartValue-100)*(-1)],
-        backgroundColor : [
-          "orange"
-        ]
-    }],
-
-    // These labels appear in the legend and in the tooltips when hovering different arcs
-    labels: [
-        'Full',
-        "Empty"
-    ]
-    
-
-};
   return (
-    <Container maxWidth="sm">
-      <Box component="span" m={1}>
-        <Card raised>
-          <CardContent>
-          <Typography variant="body1" color="textPrimary" component="p">
-            Bern Bahnhof #1
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Recycling
-          </Typography>
-          <Doughnut data={chartData}/>
-          </CardContent>
-          <CardActions>
-            <IconButton
-              className={clsx(classes.expand, {
-                [classes.expandOpen]: expanded
-              })}
-              onClick={handleExpandClick}
-              aria-expanded={expanded}
-              aria-label="show more"
-            >
-              <ExpandMoreIcon />
-            </IconButton>
-          </CardActions>
-          <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-              
-        </CardContent>
-      </Collapse>
-        </Card>
-      </Box>
-    </Container>
+    <Grid container spacing={3}>
+        <Grid item xs={3}>
+        <WasteCard data={data1}></WasteCard>
+        </Grid>
+        <Grid item xs={3}>
+        <WasteCard data={data2}></WasteCard>
+        </Grid>
+        <Grid item xs={3}>
+        <WasteCard data={data3}></WasteCard>
+        </Grid>
+        <Grid item xs={3}>
+        <WasteCard data={data4}></WasteCard>
+        </Grid>
+      </Grid>
+
+        
+
+        
+
+        
+    
   );
 };
 
